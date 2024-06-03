57a9b98b49e393fa9fac0f18255f0845b33b4b7a
Kapsulon <theo.bary@epitech.eu>
2024-06-03 15:29:44 +0200

    ADD: relative=false

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,8 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-            base: process.env.NODE_ENV === 'production' ? '/Zappy' : '',
+            base: process.env.NODE_ENV === 'production' ? '/Zappy-doc' : '',
+			relative: false
         }
 	}
 };
