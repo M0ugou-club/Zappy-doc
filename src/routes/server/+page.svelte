097488f0c03aaef0f05676db665519edb1874fbb
PaulArbez <paul.arbez@epitech.eu>
2024-06-20 18:04:32 +0200

    ADD: distrib and start egg

--- src/routes/server/+page.svelte
+++ src/routes/server/+page.svelte
@@ -14,5 +14,13 @@
         <span>
             Each time a client attempts to connect to the server, a new connection is created, this connection contains a character buffer, each time data is sent from the client to the server, it gets added to this buffer until a \n is found, the buffer is then interpreted by the server and execute its corresponding action if found.
         </span>
+        <h1 class="h2 py-4"><b>Distribution of items</b></h1>
+        <span>
+            For each item, the program adds the items one by one to random tiles on the map, taking care not to exceed the maximum possible number for each item. This maximum number is calculated according to this formula: "map_width * map_height * density".
+        </span>
+        <h1 class="h2 py-4"><b>Eggs</b></h1>
+        <span>
+            The eggs are distributed in the same way as the items, but ...
+        </span>
     </div>
 </div>
